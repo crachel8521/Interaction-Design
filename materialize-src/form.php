8a9371816f84d61b976e012a793e43b548e6faf1
TWashing10 <twashi26@uncc.edu>
2017-03-28 17:33:32 -0400

    php/validation

---
+++ materialize-src/form.php
@@ -0,0 +1,28 @@
+<?php
+       echo '<link rel="stylesheet" type="text/css" href="css/custom.css">';
+
+if(isset($_POST['submit'])){
+  $first_name = $_POST['first_name'];
+	$last_name = $_POST['last_name'];
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$address = $_POST['address'];
+	$email = $_POST['email'];
+	$telephone = $_POST['telephone'];
+	$email_updates = $_POST['email_updates'];
+  $message = "Name: " . $first_name . $last_name . "\r\nTelephone: " . $telephone . "\r\Email: " .  $email .  ;
+
+    //Makes a var named myfile and makes a file named servicerequest.txt
+    $myfile = fopen("servicerequest.txt", "w") or die("Unable to open file!");
+    //This is what will be written to the file
+    fwrite($myfile, $message);
+    //This closes the file
+    fclose($myfile);
+    //If all goes well this will display
+    echo "Thank you. Service Request Completed. " . $first_name;
+    }
+
+
+  echo nl2br('<br/><a href="dashboard.html">Go back to the main page</a>.');
+
+?>
