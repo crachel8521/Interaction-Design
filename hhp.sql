f81496c4e92676393747f96e9ea31f302ee8a45f
Harsh Akotiya <harshakotiya.229@gmail.com>
2017-03-29 17:31:32 -0400

    Updated sql
    Updated Mentor and builder tables

--- hhp.sql
+++ hhp.sql
@@ -25,25 +25,52 @@ CREATE TABLE Events
 (
   event_id     int not null,
   event_name   varchar(155) not null,
+  event_desc   varchar(255),
   event_type  varchar(35) not null,
   event_location varchar(155)
 );
 
 INSERT INTO Events VALUES 
-(1,'event 1','Family','Charlotte'),
-(2,'event 2','Kids','Rock hill'),
-(3,'event 3','Designer','Durham')
+(1,'event 1', 'event description comes here','Family','Charlotte'),
+(2,'event 2', 'event description comes here','Kids','Rock hill'),
+(3,'event 3', 'event description comes here','Designer','Durham')
 ;
 
 CREATE TABLE Stories
 (
   story_id     int not null,
   story_title   varchar(155) not null,
-  user_id  int not null
+  user_id  int not null,
+  story_text varchar(500),
+  Data_posted Datetime
 );
 
 INSERT INTO Stories VALUES 
-(1,'Story 1', 3),
-(2,'Story 2', 2),
-(3,'Story 3', 1)
+(1,'Story 1', 3, 'Story text comes here', now()),
+(2,'Story 2', 2, 'Story text comes here', now()),
+(3,'Story 3', 1, 'Story text comes here', now())
+;
+
+CREATE TABLE mentor
+(
+  user_id     int not null,
+  mentee_name varchar(100) not null,
+  mentor_sname  varchar(50) not null,
+  mentee_sname  varchar(50) not null
+);
+
+INSERT INTO mentor VALUES 
+(3,'Ralph Simonian', 'ralph123', 'jones22')
+;
+
+CREATE TABLE builder
+(
+  user_id     int not null,
+  builder_name varchar(100) not null,
+  device_recipient_id int not null,
+  builder_sname  varchar(50) not null
+);
+
+INSERT INTO builder VALUES 
+(4,'Steven Smith', 1095, 'Steve49')
 ;
