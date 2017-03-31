26eee8d547a150132da7cf51424aa2139892d7be
crachel8521 <chesley.r.m@gmail.com>
2017-03-31 07:32:14 -0400

    Updates

--- materialize-src/model/database.php
+++ materialize-src/model/database.php
@@ -1,12 +1,16 @@
 <?php
-	$database = 'jasonh11_Pubfinder';
-	$host = 'localhost';
-    $dsn = 'mysql:host=localhost;dbname=jason11_Pubfinder';
-    $dbUserName = 'jasonh11_pubfind';
-    $password = 'pa55word';
+	$database = 'id1201354_hhp';
+	$host = 'ns01.000webhost.com';
+    $dsn = 'mysql:host=id1201354_forum;dbname=id1201354_hhp';
+    $dbUserName = 'id1201354_crachel';
+    $password = 'rachelm8';
+
+    echo "Test Connection: ";
 
     try {
-        $db = new PDO("mysql:host=$host; dbname=$database", $dbUserName, $password);
+        $conn = new PDO("mysql:host=$host; dbname=$database", $dbUserName, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
         include('errors/database_error.php');
