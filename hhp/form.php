a20fd2984a2a5268ba6f9c6078045227e74c737e
TWashing10 <twashi26@uncc.edu>
2017-04-06 16:01:30 -0400

    form

--- hhp/form.php
+++ hhp/form.php
@@ -2,37 +2,42 @@
       session_start();
 
       include 'view/header.php';
+      include 'model/database.php';
       echo '<link rel="stylesheet" type="text/css" href="css/custom.css">';
+?>
+
+<?php
 
 function addUser()
 {
 if(isset($_POST['submit'])){
     session_start();
   }
-  $password = $_POST['password'];
-  $password2 = $_POST['password2'];
-  $last_name = $_POST['last_name'];
-  $first_name = $_POST['first_name'];
-	$username = $_POST['username'];
-	$address = $_POST['address'];
-	$email = $_POST['email'];
-	$email_updates = $_POST['email_updates'];
-  $message = "Name: " . $first_name .' ' . $last_name . "<br> Email: " .  $email . " <br>Username: " .$username . "<br> Address: " . $address;
+  $password = mysql_real_escape_string($_POST['password']);
+  $password2 = mysql_real_escape_string($_POST['password2']);
+  $last_name = mysql_real_escape_string($_POST['last_name']);
+  $first_name =  mysql_real_escape_string($_POST['first_name']);
+	$username = mysql_real_escape_string($_POST['username']);
+	$address = mysql_real_escape_string($_POST['address']);
+	$email = mysql_real_escape_string($_POST['email']);
+	$email_updates = mysql_real_escape_string($_POST['email_updates']);
+  $message = "Name: " . $first_name .' ' . $last_name . "<br> Email: " .  $email . " <br> Username: " .$username . "<br> Address: " . $address;
 
-if($password == $password2){
+  $checkusername = mysql_query("SELECT * FROM Accounts WHERE Username = '".$username."'");
+if($checkusername < 1){
 
   $query = 'INSERT INTO Accounts
                (user_id, password, last_name, first_name, username, email, age, address, email_updates)
             VALUES
                (:user_id, :password, :last_name, :first_name, :username, :email, :age, :address, :email_updates)';
 
- $db =   $db = new PDO("mysql:host=localhost;dbname=hhp","root","");
+ $db = new PDO("mysql:host=localhost;dbname=hhp","root","");
  $statement = $db->prepare($query);
  echo $message;
- echo "<br> Thank you. Your Registration Has Been Completed " . $first_name;
+ echo "<br> Thank you! Your Registration Has Been Completed, " . $first_name;
  echo " <br> In A Few Moments, You Should Be Receiving An E-mail To: " . $email;
 }else{
- echo "Sorry. Your Passwords Do Not Match";
+ echo "Sorry.  This Username Already Exists.  Please Try Again.";
 }
   }
 
