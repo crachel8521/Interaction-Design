f176d8b942f459f84dd277ae4c5f980a8389e002
crachel8521 <chesley.r.m@gmail.com>
2017-04-18 17:50:17 -0400

    HHP SQL Update

--- hhp.sql
+++ hhp.sql
@@ -7,7 +7,7 @@ USE hhp;
 
 CREATE TABLE Accounts 
 (
-user_id     int not null auto_increment primary key,
+user_id     int auto_increment,
 user_name   varchar(155)not null,   
 password    varchar(35) not null,   
 last_name  varchar(35) not null,   
@@ -16,7 +16,8 @@ user_sname  varchar(50),
 email       varchar(35) not null,   
 address     varchar(155),   
 age     	int not null,   
-user_type   varchar(35) 
+user_type   varchar(35), 
+CONSTRAINT PK_Person PRIMARY KEY (user_id,user_name)
 );
 
 
@@ -29,43 +30,52 @@ INSERT INTO Accounts VALUES
 
 CREATE TABLE Events
 (
-  event_id     int not null,
+  event_id     int auto_increment,
   event_name   varchar(155) not null,
   event_desc   varchar(255),
   event_type  varchar(35) not null,
   event_location varchar(155),
-  event_link   varchar(245) not null
+  event_photo  blob,
+  event_link   varchar(245) not null,
+  CONSTRAINT PK_Event PRIMARY KEY (event_id)
 );
 
-INSERT INTO Events VALUES 
-(1,'HHP Get Together', 'event description comes here','Family','Charlotte', 'Event link comes here'),
-(2,'HHP Picnic', 'event description comes here','Kids','Rock hill', 'Event link comes here'),
-(3,'HHP Builder Meeting', 'event description comes here','Designer','Durham', 'Event link comes here')
+INSERT INTO Events VALUES
+(1,'Demo, User Studies', 'Students will submit their user study progress report and demonstrate their project development','Class','Charlotte', '', 'https://uncc.instructure.com/courses/46603/assignments/167853'),
+(2,'Grad Scholarly Article', 'Graduate students will submit their research based on their projects','Graduate Students','Charlotte', '', 'https://uncc.instructure.com/courses/46603/assignments/167853'),
+(3,'Final Demo', 'Students will demonstrate their final functional project','Class','Charlotte', '', 'https://uncc.instructure.com/courses/46603/assignments/167853')
 ;
+select * from Events;
 
+DROP TABLE IF EXISTS Stories;
 CREATE TABLE Stories
 (
-  story_id     int not null,
+  story_id     int auto_increment,
   story_title   varchar(155) not null,
-  user_name   varchar(155)not null,
-  story_text varchar(500),
+  user_id  int not null,
+  story_text varchar(1000),
+  story_photo  blob,
   date_posted Datetime,
-  likes int not null
+  likes int not null,
+  CONSTRAINT PK_Story PRIMARY KEY (story_id),
+  CONSTRAINT FK_Story FOREIGN KEY (user_id) REFERENCES Accounts(user_id)
 );
 
 INSERT INTO Stories VALUES 
-(1,'Story 1', 3, 'Story text comes here', now(), 10),
-(2,'Story 2', 2, 'Story text comes here', now(), 15),
-(3,'Story 3', 1, 'Story text comes here', now(), 20)
+(1,'Hamed’s Genium story', 3, 'Living in a big city, Hamed spends a lot of time walking and being outdoors. He depends on the Genium prosthetic leg to help him stay active with exercise and keep up with his friends and his career. As a prosthetist, Hamed knows the importance of a high functioning prosthesis like the Genium prosthetic leg.', '',now(),2),
+(2,'Josh’s DynamicArm story', 2, 'Josh’s drive and passion are reflected in every area of his life. Whether he is knotting his tie in preparation for handling a legal case or loosening up for a 3-point shot from the arc, he uses his DynamicArm to the fullest.', '',now(),5),
+(3,'Andrew and the X3 waterproof prosthetic leg', 1, 'Lt. Col. (Ret.) Andrew Lourake was the first Air Force pilot with an above-knee amputation to return to active duty. He did it on a C-Leg, and has since moved on to the X3, Ottobock’s remarkable waterproof microprocessor knee. In addition to jetting around lakes, he takes to the sky and works at the Veterans Air Command (VAC), an amazing organization that provides free air transportation to post 9/11 combat wounded and their families for medical and other compassionate purposes through a network of volunteer aircraft owners and pilots.', '',now(), 4)
 ;
+select * from stories;
 
 CREATE TABLE Mentor
 (
   user_id int not null,
   mentor_id int not null,
   mentee_id int not null,
-  mentorship_type varchar(35) not null
-);
+  mentorship_type varchar(35) not null,
+  CONSTRAINT FK_Mentor FOREIGN KEY (user_id) REFERENCES Accounts(user_id)
+  );
 
 INSERT INTO Mentor VALUES 
 (1, 1, 2, 'mentor'),
@@ -74,14 +84,17 @@ INSERT INTO Mentor VALUES
 
 CREATE TABLE Builder_Status
 (
-  builder_user_id    int not null,
-  recipient_user_id    int not null,
+  builder_user_id    int,
+  recipient_user_id    int,
   builder_name varchar(100) not null,
-  status_update  blob(65535),
+  status_update  blob,
   status_desc varchar(500),
-  date_posted datetime not null
+  date_posted datetime not null,
+  CONSTRAINT FK_Builder FOREIGN KEY (builder_user_id) REFERENCES Accounts(user_id),
+  CONSTRAINT FK_Recipient FOREIGN KEY (recipient_user_id) REFERENCES Accounts(user_id)
 );
 
 INSERT INTO Builder_Status VALUES 
-(4, 2,'Steven Smith', '','status update comes here', now())
+(4, 2,'Steven Smith', '','Hand is in the designing process', now())
 ;
+select * from builder_status;
