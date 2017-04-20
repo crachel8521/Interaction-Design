d5a00f47258535bbb0bb63d9ac28db354c7f2dd3
Harsh Akotiya <harshakotiya.229@gmail.com>
2017-04-19 22:36:49 -0400

    merged status timeline in dashboard

--- hhp/dashboard.php
+++ hhp/dashboard.php
@@ -49,14 +49,99 @@ $mentorship_type = getMentorshipType($_SESSION['user_id']);
 
 
               <div class="col s12">
-                <div class="card cyan darken-1">
-                  <div class="card-content white-text">
+                <div class="card cyan darken-1" >
+                  <div class="card-content white-text" style=" height:140px;">
                     <span class="card-title">Build</span>
                     <a class="waves-effect waves-light btn purple">Request a Device</a>
                     <a class="waves-effect waves-light btn purple">Build a Device</a>
                     <br><br>
                     <span class="card-title">Project</span>
                   </div>
+					<div class="container">
+    		<div class="row">
+				<div class="col-md-12" style="padding-right: 0px;padding-left: 0px;">
+					<div class="page-header">
+					  <h4 style="line-height: 0%;color:white;">Build Status</h4>
+					</div>
+					<div style="display:inline-block;width:100%;overflow-y:auto;">
+					<ul class="timeline timeline-horizontal">
+						<li class="timeline-item">
+							<div class="timeline-badge primary"><i class="glyphicon glyphicon-check"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h4 style="font-size: 1.28rem;line-height: 15%;" class="timeline-title">Mussum ipsum cacilds 1</h4>
+									<p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 5 days ago via Builder</small></p>
+								</div>
+								<div class="timeline-body">
+									<p>Mussum ipsum cacilds, vidis litro abertis. Consetis faiz elementum girarzis, nisi eros gostis.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-item">
+							<div class="timeline-badge success"><i class="glyphicon glyphicon-check"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h4 style="font-size: 1.28rem;line-height: 15%;" class="timeline-title">Mussum ipsum cacilds 2</h4>
+									<p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 3 days ago via Builder</small></p>
+								</div>
+								<div class="timeline-body">
+									<p>Mussum ipsum cacilds, vidis faiz elementum girarzis, nisi eros gostis.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-item">
+							<div class="timeline-badge info"><i class="glyphicon glyphicon-check"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h4 style="font-size: 1.28rem;line-height: 15%;" class="timeline-title">Mussum ipsum cacilds 3</h4>
+									<p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 1 day ago via Builder</small></p>
+								</div>
+								<div class="timeline-body">
+									<p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipisci. Mé faiz elementum girarzis, nisi eros gostis.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-item">
+							<div class="timeline-badge danger"><i class="glyphicon glyphicon-check"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h4 style="font-size: 1.28rem;line-height: 15%;" class="timeline-title">Mussum ipsum cacilds 4</h4>
+									<p><small class="text-muted"><i class="glyphicon glyphicon-time"></i>10 hours ago via Builder</small></p>
+								</div>
+								<div class="timeline-body">
+									<p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-item">
+							<div class="timeline-badge warning"><i class="glyphicon glyphicon-check"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h4 style="font-size: 1.28rem;line-height: 15%;" class="timeline-title">Mussum ipsum cacilds 5</h4>
+									<p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 6 hours ago via Builder</small></p>
+								</div>
+								<div class="timeline-body">
+									<p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis.</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-item">
+							<div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h4 style="font-size: 1.28rem;line-height: 15%;" class="timeline-title">Mussum ipsum cacilds 6</h4>
+									<p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 3 hours ago via Builder</small></p>
+								</div>
+								<div class="timeline-body">
+									<p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis.</p>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+				</div>
+			</div>
+		</div>
                 </div>
               </div>
           </div>
