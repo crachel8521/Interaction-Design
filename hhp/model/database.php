b6d32d733aea5f16f4e0da56e976593d8235bf9d
TWashing10 <twashi26@uncc.edu>
2017-04-25 14:35:42 -0400

    ??

--- hhp/model/database.php
+++ hhp/model/database.php
@@ -10,7 +10,7 @@
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
-				include('errors/database_error.php');
+			include('errors/database_error.php');
         exit();
     }
 ?>
